b07bba71099860b31fd35d2f9e1b31ec120c3d38
kharva <parvadesai404@gmail.com>
2024-04-26 22:34:07 +0530

    [Add] contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     fontFamily: {
       'systemui': ["system-ui", "sans-serif"],
@@ -12,11 +12,13 @@ module.exports = {
     colors: {
       "mainbg-color": "rgb(249, 248, 244)",
       "mainbg-color-dark": "rgb(30, 30, 30)",
-      "font-color": "rgb(55, 53, 47)",
+      "font-color-pri": "rgb(55, 53, 47)",
+      "font-color-sec": "#20C20E",
+      "font-color-ter": "#FFFFFF",
       "font-color-dark": "rgb(235, 235, 235)",
       "link-color": "rgb(232, 28, 79)",
       "hover-color": "rgb(108, 117, 125)",
-      "navbar-color": "rgb(244, 242, 233)",
+      "navbar-color-pri": "rgb(244, 242, 233)",
       "navbar-color-dark": "rgb(18, 18, 18)",
     },
     
