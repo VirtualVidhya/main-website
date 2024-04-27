3717ac3aa80bac4ee04ba46bad729834244c4ef1
kharva <parvadesai404@gmail.com>
2024-04-27 20:14:35 +0530

    [Add] courses page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ["./src/**/*.{html,js}", "./*.html", "./courses/*.html"],
   theme: {
     fontFamily: {
       'systemui': ["system-ui", "sans-serif"],
@@ -15,14 +15,21 @@ module.exports = {
       "font-color-pri": "rgb(55, 53, 47)",
       "font-color-sec": "#20C20E",
       "font-color-ter": "#FFFFFF",
+      "font-color-qua": "#55aff9",
       "font-color-dark": "rgb(235, 235, 235)",
       "link-color": "rgb(232, 28, 79)",
       "hover-color": "rgb(108, 117, 125)",
       "navbar-color-pri": "rgb(244, 242, 233)",
       "navbar-color-dark": "rgb(18, 18, 18)",
+      "color-transparent": "rgb(0,0,0,0)",
+      "color-border-pri": "rgb(156, 154, 151)"
     },
-    
-    extend: {},
+
+    extend: {
+      borderWidth: {
+        '6': "6px"
+      },
+    },
   },
   plugins: [],
 }
