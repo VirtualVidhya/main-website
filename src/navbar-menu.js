23e6c4c3fd1c0c06ff0ef1039100b516b8131670
kharva <parvadesai404@gmail.com>
2024-04-28 21:10:18 +0530

    [Fix] nav-bar menu design
    spacing issues + underline offset, fixed logo link redirection on diff
    courses pages

--- src/navbar-menu.js
+++ src/navbar-menu.js
@@ -4,9 +4,11 @@ function Menu(e) {
     e.name === "menu"
       ? ((e.name = "close"),
         list.classList.add("top-[45px]"),
-        list.classList.add("opacity-100"))
+        list.classList.add("opacity-100"),
+        list.classList.add("py-4"))
       : ((e.name = "menu"),
         list.classList.remove("top-[45px]"),
-        list.classList.remove("opacity-100"));
+        list.classList.remove("opacity-100"),
+        list.classList.remove("py-4"));
   }
   
