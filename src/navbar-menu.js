393154efa06f3c9a2208f9b8566479c7347b8420
kharva <parvadesai404@gmail.com>
2024-12-19 21:12:37 +0530

    [Update] side nav-bar menu

--- src/navbar-menu.js
+++ src/navbar-menu.js
@@ -7,7 +7,7 @@ function Menu(e) {
       e.classList.remove("fa-bars");
       e.classList.add("fa-xmark");
 
-      list.classList.add("translate-x-[400px]");
+      list.classList.add("translate-x-[640px]");
       list.classList.add("opacity-100");
     } 
     else {
@@ -16,7 +16,7 @@ function Menu(e) {
       e.classList.add("fa-bars");
       e.classList.remove("fa-xmark");
 
-      list.classList.remove("translate-x-[400px]");
+      list.classList.remove("translate-x-[640px]");
       list.classList.remove("opacity-100");
     }
 }
