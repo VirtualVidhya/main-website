0ec7cbf40b4db9a61e8a4c20ea62cf0c8c7ca208
kharva <parvadesai404@gmail.com>
2025-01-30 22:07:22 +0530

    [Update] optimized lottie-anim loading

--- src/scripts/specific/lottie-anim.js
+++ src/scripts/specific/lottie-anim.js
@@ -1,12 +1,31 @@
 // lottie.js
 // import { DotLottie } from "@lottiefiles/dotlottie-web";
-import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
+// import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
 
-window.loadLottieAnimation = function (elementId, animationSrc) {
-  const dotLottie = new DotLottie({
-    autoplay: true,
-    loop: true,
-    canvas: document.querySelector(`#${elementId}`),
-    src: animationSrc,
+// window.loadLottieAnimation = function (elementId, animationSrc) {
+//   const dotLottie = new DotLottie({
+//     autoplay: true,
+//     loop: true,
+//     canvas: document.querySelector(`#${elementId}`),
+//     src: animationSrc,
+//   });
+// };
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const { DotLottie } = await import(
+    "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm"
+  );
+
+  window.loadLottieAnimation = function (elementId, animationSrc) {
+    const dotLottie = new DotLottie({
+      autoplay: true,
+      loop: true,
+      canvas: document.querySelector(`#${elementId}`),
+      src: animationSrc,
+    });
+  };
+
+  document.querySelectorAll("[data-lottie]").forEach((canvas) => {
+    window.loadLottieAnimation(canvas.id, canvas.dataset.lottie);
   });
-};
+});
