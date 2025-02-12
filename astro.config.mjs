7173b58d6be647fd29111d4d615eb03c34bd0107
kharva <parvadesai404@gmail.com>
2025-02-13 00:00:45 +0530

    [Add] file minification & compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,29 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import compress from '@playform/compress';
+import compressor from 'astro-compressor';
 
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind(),
+        compress(
+            {
+                CSS: true,
+                HTML: {
+                  'html-minifier-terser': {
+                    removeAttributeQuotes: false,
+                  },
+                },
+                Image: true,
+                JavaScript: true,
+                SVG: true,
+              }
+        ),
+        compressor({
+            fileExtensions: [".html", ".css", ".js", ".mjs", ".svg"],
+            gzip: true,
+            brotli: true,
+        })
+    ],
     output: "static",
 });
