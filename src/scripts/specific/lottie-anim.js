f857e32493a0cc0a69bbf2cc6f2262b788a23392
kharva <parvadesai404@gmail.com>
2025-02-15 03:42:01 +0530

    [Update] lottie anim loading

--- src/scripts/specific/lottie-anim.js
+++ src/scripts/specific/lottie-anim.js
@@ -30,24 +30,55 @@
 //   });
 // });
 
-(async () => {
-  // Preload DotLottie module immediately
-  const { DotLottie } = await import(
-    "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm"
-  );
+// (async () => {
+//   // Preload DotLottie module immediately
+//   const { DotLottie } = await import(
+//     "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm"
+//   );
 
-  // Define the function globally
-  window.loadLottieAnimation = function (elementId, animationSrc) {
-    const dotLottie = new DotLottie({
+//   // Define the function globally
+//   window.loadLottieAnimation = function (elementId, animationSrc) {
+//     const dotLottie = new DotLottie({
+//       autoplay: true,
+//       loop: true,
+//       canvas: document.querySelector(`#${elementId}`),
+//       src: animationSrc,
+//     });
+//   };
+
+//   // Start loading animations right away
+//   document.querySelectorAll("[data-lottie]").forEach((canvas) => {
+//     window.loadLottieAnimation(canvas.id, canvas.dataset.lottie);
+//   });
+// })();
+
+(async () => {
+  function loadLottie() {
+    // return import("https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm");
+    return import("https://esm.sh/@lottiefiles/dotlottie-web");
+  }
+
+  async function loadAnimation(elementId, animationSrc) {
+    const { DotLottie } = await loadLottie();
+    new DotLottie({
       autoplay: true,
       loop: true,
       canvas: document.querySelector(`#${elementId}`),
       src: animationSrc,
     });
-  };
+  }
 
-  // Start loading animations right away
-  document.querySelectorAll("[data-lottie]").forEach((canvas) => {
-    window.loadLottieAnimation(canvas.id, canvas.dataset.lottie);
-  });
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(async (entry) => {
+        if (entry.isIntersecting) {
+          await loadAnimation(entry.target.id, entry.target.dataset.lottie);
+          observer.unobserve(entry.target); // Load only once
+        }
+      });
+    },
+    { rootMargin: "100px" }
+  );
+
+  document.querySelectorAll("[data-lottie]").forEach((el) => observer.observe(el));
 })();
