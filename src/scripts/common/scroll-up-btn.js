5e5509172cadac5dfe85064adcb225df9ae447e8
kharva <parvadesai404@gmail.com>
2025-02-15 21:43:47 +0530

    Squash merge main into dev

--- src/scripts/common/scroll-up-btn.js
+++ src/scripts/common/scroll-up-btn.js
@@ -1,16 +1,21 @@
-const btn = document.getElementById("scroll-up-btn");
+function setupScrollButton() {
+  const btn = document.getElementById("scroll-up-btn");
+  if (!btn) return;
 
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100 || window.scrollY > 100) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-});
-
-btn.addEventListener("click", () => {
-  document.documentElement.scrollTo({
-    top: 0,
-    behavior: "smooth",
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100 || window.scrollY > 100) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
   });
-});
+
+  btn.addEventListener("click", () => {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+setupScrollButton();
